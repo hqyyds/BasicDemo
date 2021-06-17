89c256b09854d40fe592397f97c0dba789812224
hqyyd <602920177@qq.com>
2021-06-17 18:26:27 +0800

    补充

--- src/main/java/com/h3w/service/impl/UserServiceImpl.java
+++ src/main/java/com/h3w/service/impl/UserServiceImpl.java
@@ -158,12 +158,6 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getById(Integer id) {
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println("线程名：" + Thread.currentThread().getName());
         return userDao.getByHQL("from User where id=?1",id);
     }
 
